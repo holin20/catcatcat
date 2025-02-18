5833bc69a6728df44bbf7832c8b3ceeb4cf4aed6
Ho-ling Chang <holin20@outlook.com>
2025-02-18 07:16:27 +0000

    Run monitor script colorfully

--- scripts/run_monitor.sh
+++ scripts/run_monitor.sh
@@ -3,5 +3,5 @@
 export CATCATCAT_GAPP_PW=$(cat secrets/google_app_password.txt)
 mkdir -p logs
 
-#go run cmd/monitor/main.go 2>&1 | jq -c 'del(.caller)'
-go run cmd/monitor/main.go
+go run cmd/monitor/main.go 2>&1 | jq -c 'del(.caller)'
+#go run cmd/monitor/main.go
